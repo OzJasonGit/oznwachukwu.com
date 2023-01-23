f150cf1c7ea7b1fd9ec2b706229be5a6bd900b78
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2023-01-23 22:55:48 +0000

    good night

--- frontend/static/portfolio-architecture.js
+++ frontend/static/portfolio-architecture.js
@@ -24,7 +24,10 @@
 
 
 
-
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
 
 const router = async () => {
     const routes = [
@@ -38,12 +41,29 @@ const router = async () => {
         return {
             route: route,
             isMatch: location.pathname === route.path
-        };
+        }
     });
 
-    console.log (potentialMatches);
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if (!match){
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    };
+
+    console.log (match.route.view());
 };
 
+window.addEventListener("popstate", router);
+
 document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e =>{
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
     router();
 });
