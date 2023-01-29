519dc6a19ef14d2ac87e493a2e968e2d5ad62385
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2023-01-29 00:51:07 +0000

    menu update

---
+++ frontend/javascript/menu-dropdown.js
@@ -0,0 +1,39 @@
+
+
+
+
+//////////////////////////MENU DROPDOWN/////////////////////////////////
+const burgerMenu = document.getElementById()
+
+function menuDrodown(){
+    let menu = 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////GITHUB BUTTON CHANGE COLOR/////////////////////
+const githubButton = document.getElementById('GITHUB-ICON')
+
+//change icon colour and add shadow on mouseover 
+
+githubButton.addEventListener('mouseover', function handleMouseOver(){
+  githubButton.style.color = 'magenta';
+});
+
+
+
+githubButton.addEventListener('mouseout', function handleMouseOver(){
+    githubButton.style.color = '#2e2e2e';
+});
+//////////////////////////GITHUB BUTTON CHANGE COLOR/////////////////////
+
